068b13e934e73bd534bea5be39bd49ee9263f695
Julien Palard <julien@palard.fr>
2021-12-02 15:51:32 +0100

    Please the dumb test...

--- exercises/dautres-exercices/check.py
+++ exercises/dautres-exercices/check.py
@@ -1,5 +1,17 @@
-print("This is not really an exercise, it has no check, #sry, there's just no way to « do » it.")
-print()
-print("Or maybe if you write a new exercise, or donate, I can make the effort of logging into the admin zone and manially validate it for you, if you care :D")
+import sys
+from correction_helper import exclude_file_from_traceback
 
-exit(1)
+exclude_file_from_traceback(__file__)
+
+print(
+    "This is not really an exercise, it has no check, "
+    "#sry, there's just no way to « do » it."
+)
+print()
+print(
+    "Or maybe if you write a new exercise, or donate, "
+    "I can make the effort of logging into the admin zone and "
+    "manually validate it for you, if you care :D"
+)
+
+sys.exit(1)
