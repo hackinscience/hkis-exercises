f3676e5ff268a2d7de016c5c9bb9e99a9cbf0995
Julien Palard <julien@palard.fr>
2021-12-05 22:16:23 +0100

    More test for sum_primes, to tell more explicit results.

--- exercises/sum-every-prime-number-below-n/check.py
+++ exercises/sum-every-prime-number-below-n/check.py
@@ -8,7 +8,74 @@ with student_code():
     from solution import sum_primes
 
 
-def check():
+def test_is_increasing():
+    prev = None
+    for i in range(1, 100, 2):
+        with student_code(
+            prefix=[
+                "While testing your `sum_primes` function as:",
+                code(f"sum_primes({i})", "python"),
+            ]
+        ):
+            current = sum_primes(i)
+        if prev is not None:
+            if current < prev:
+                fail(
+                    f"`sum_prime({i})` can only be greater or equal to "
+                    f"`sum_prime({i-1})`.",
+                    "Your implemtation gave:",
+                    "- `sum_primes({i-1}) → {prev!r}\n"
+                    "- `sum_primes({i}) → {current!r}\n",
+                )
+        prev = current
+
+
+def test_some_small_obvious_ones():
+    # Based on first primes : 2, 3, 5, 7, 11
+    with student_code(
+        prefix=[
+            "While testing your `sum_primes` function as:",
+            code("sum_primes(2)", "python"),
+        ]
+    ):
+        result = sum_primes(2)
+    if result != 0:
+        fail(
+            "`sum_primes(2)` is expected to give `0` "
+            "because there's no prime number less than two.",
+            f"Your function returned: `{result!r}`.",
+        )
+    with student_code(
+        prefix=[
+            "While testing your `sum_primes` function as:",
+            code("sum_primes(4)", "python"),
+        ]
+    ):
+        result = sum_primes(4)
+    if result != 5:
+        fail(
+            "`sum_primes(4)` is expected to give `5` "
+            "because there's the prime numbers `2` and `3` "
+            "being less than `4`, and `2+3` gives `5`.",
+            f"Your function returned: `{result!r}`.",
+        )
+    with student_code(
+        prefix=[
+            "While testing your `sum_primes` function as:",
+            code("sum_primes(12)", "python"),
+        ]
+    ):
+        result = sum_primes(12)
+    if result != 2 + 3 + 5 + 7 + 11:
+        fail(
+            f"`sum_primes(12)` is expected to give `{2+3+5+7+11}` "
+            "because there's the prime numbers `2`, `3`, `5`, `7`, and `11` "
+            f"being less than `12`, and `2+3+5+7+11` gives `{2+3+5+7+11}`.",
+            f"Your function returned: `{result!r}`.",
+        )
+
+
+def test_known_values():
     for i, expected in CHECKS.items():
         with student_code(
             prefix=[
@@ -26,4 +93,6 @@ def check():
 
 
 if __name__ == "__main__":
-    check()
+    test_some_small_obvious_ones()
+    test_is_increasing()
+    test_known_values()
